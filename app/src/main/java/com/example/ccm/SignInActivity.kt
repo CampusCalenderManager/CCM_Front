381df52923261ba4a11ae10722c8067fd0cc3ffb
uinone <tizm423@gmail.com>
2022-05-26 04:08:01 +0900

    [feat] Login done

--- app/src/main/java/com/example/ccm/SignInActivity.kt
+++ app/src/main/java/com/example/ccm/SignInActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ccm
 
 import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,7 +10,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.example.ccm.API.APISignIn
+import com.example.ccm.API.LoginUserDataJSON
 import com.example.ccm.API.SignInJSON
+import com.example.ccm.CCMApp.Companion.userLocalDB
+import com.example.ccm.LocalDB.User
+import com.example.ccm.databinding.ActivityMainBinding
+import com.example.ccm.databinding.ActivitySignInBinding
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,19 +24,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val loginButton = findViewById<Button>(R.id.login_button)
-
-        loginButton.setOnClickListener {
+        binding.loginButton.setOnClickListener {
             val userNameInput = findViewById<EditText>(R.id.login_id_input)
             val passwordInput = findViewById<EditText>(R.id.login_password_input)
-
             callSignInAPI(userNameInput.text.toString(), passwordInput.text.toString())
+        }
 
-            // Toast.makeText(this, CCMApp.prefs.token, Toast.LENGTH_SHORT).show()
+        binding.loginSignUpButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish() // Todo : 회원가입 창에서 뒤로가기 누르면 끝남 이를 방지하자
         }
     }
 
@@ -44,23 +56,56 @@ class SignInActivity : AppCompatActivity() {
         val apiAddSchedule = retrofit.create(APISignIn::class.java)
 
         apiAddSchedule.postSignIn(
-            SignInJSON (
-                userName,
-                password
-            )
-        ).enqueue(object : Callback<Void> {
-            override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                Log.d("login POST", "${response.code()}")
-
+            SignInJSON (userName, password)
+        ).enqueue(object : Callback<LoginUserDataJSON> {
+            override fun onResponse(
+                call: Call<LoginUserDataJSON>,
+                response: Response<LoginUserDataJSON>
+            ) {
                 // 로그인이 정상적으로 됐다면 토큰을 저장
                 if (response.code() == 200) {
-                    // CCMApp.prefs.token = response.headers().values("accesstoken")[0]
+                    val userToken = response.headers().values("AccessToken")[0]
+                    val userData = response.body()
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val users = CoroutineScope(Dispatchers.IO).async {
+                            userLocalDB.userDao().getAll()
+                        }.await()
+
+                        users[0].userToken = userToken
+                        users[0].username = userData!!.name
+
+                        CoroutineScope(Dispatchers.IO).async {
+                            userLocalDB.userDao().update(users[0])
+                        }.await()
+
+                        Toast.makeText(binding.root.context, "반갑습니다 ${users[0].username} 님", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(binding.root.context, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                } else {
+                    // response code 가 200이 아니라면 실패 메시지 출력
+                    Toast.makeText(binding.root.context, "로그인에 실패했어요", Toast.LENGTH_LONG).show()
+
+                    val intent = Intent(binding.root.context, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
                 }
             }
 
-            override fun onFailure(call: Call<Void>, t: Throwable) {
+            override fun onFailure(call: Call<LoginUserDataJSON>, t: Throwable) {
                 Log.d(ContentValues.TAG, "실패 : $t")
+                Toast.makeText(binding.root.context, "요청이 잘못됐어요", Toast.LENGTH_LONG).show()
             }
         })
+    }
+
+    // 뒤로가기 누르면 메인 액티비티 생성
+    override fun onBackPressed() {
+        val intent = Intent(binding.root.context, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
