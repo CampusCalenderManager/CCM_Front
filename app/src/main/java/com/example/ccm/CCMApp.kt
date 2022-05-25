361d0153b2829f2daa2dee89d7d958ec80873512
uinone <tizm423@gmail.com>
2022-05-25 13:32:55 +0900

    Modify ccmApp.class -> CCMApp.class, Add ViewBinding

--- app/src/main/java/com/example/ccm/CCMApp.kt
+++ app/src/main/java/com/example/ccm/CCMApp.kt
@@ -3,7 +3,10 @@ package com.example.ccm
 import android.app.Application
 import com.example.ccm.API.Prefs
 
-class ccmApp : Application() {
+/**
+ * 휴대전화 자체에 데이터를 저장하기 위한 싱글톤 클래스
+ */
+class CCMApp : Application() {
     companion object{
         lateinit var prefs: Prefs
     }
